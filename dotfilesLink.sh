7d852ddea782b5972750a6852b3eb1876f80adb5
gi <2357gi>
2018-09-22 00:39:13 +0900

    .vim/をgitで管理

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # from @2357gi
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.cheatsheet.md ~/.cheatsheet.md
+ln -sf ~/dotfiles/.vim ~/.vim
 
