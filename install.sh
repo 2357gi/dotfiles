129cec7d0acc646f3a67ee5f11b401a82ab064ac
2357gi <2357.gi@gmail.com>
2019-01-25 16:21:56 +0900

    [add] install.shにリンク貼るやつ追加

--- install.sh
+++ install.sh
@@ -1,1 +1,35 @@
 # its one line installer
+
+set -eu
+
+cat << END
+
+**************************************************
+DOTFILES SETUP START!
+**************************************************
+
+END
+
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+
+echo "start setup..."
+for f in .??*; do
+	[ "$f" = ".git" ] && continue
+	[ "$f" = ".gitconfig.local.template" ] && continue
+
+	ln -snfv ~/dotfiles/"$f" ~/
+done
+
+[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig
+
+
+cat << END
+
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+
+END
